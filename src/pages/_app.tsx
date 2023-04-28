b93c465366beb1af0ca8a37d34242a90232850fc
Guilherme Teixeira <80058972+Guui-t@users.noreply.github.com>
2023-04-27 23:05:00 -0300

    changed useEffect local

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
+import { useMembers } from '@/hooks/useMembers'
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  const { getMembers } = useMembers()
+  
+  useEffect(() => {
+    getMembers()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <ChakraProvider>
       
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,8 @@
 
 import { MemberList } from "@/components/MemberList";
-import { useMembers } from "@/hooks/useMembers";
-import { useEffect } from "react";
 
 export default function Home() {
-  const { getMembers } = useMembers()
   
-  useEffect(() => {
-    getMembers()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
   return (
     <>
       <MemberList />
